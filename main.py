988801cc39b44985a35160fe22cc95f98bc9f240
Alemnew Marie <alemnewmarie461@gmail.com>
2021-08-12 14:56:00 +0300

    Scraping the first page appeared on search result

--- main.py
+++ main.py
@@ -3,48 +3,54 @@ Indeed job notifier is a python script that automates job searching on
 www.indeed.com and send notification email to the user when a new job 
 matching the user's profile is posted.
 
+It checks for new job posts every 3 days.
+
 Author: @balewgize
 Date: August, 2013 E.C
 """
 
-import sys
 import csv
 import requests
-from datetime import date, datetime
+from datetime import datetime
 from bs4 import BeautifulSoup
-from requests.api import post
 
 import job_filter
 
 
 def basic_search():
     """ Perform basic job search based on postion and location."""
-    position = job_filter.get_position()
-    location = job_filter.get_location()
+    # position = job_filter.get_position()
+    # location = job_filter.get_location()
+    position = '"Python Developer"'
+    location = 'Remote'
+    sort_by = 'date'
+    last_three_days = '3'
 
     params = (
         ('q', position),
         ('l', location),
+        ('sort', sort_by),
+        ('fromage', last_three_days),
     )
     response = requests.get('https://www.indeed.com/jobs', params=params)
     return response
 
 
-def advanced_search():
-    """ Perform advanced job searching by filtering using different parameters."""
-    position = job_filter.get_position()
-    location = job_filter.get_location()
-    job_type = job_filter.get_job_type()
-    experience = job_filter.get_experience()
+# def advanced_search():
+#     """ Perform advanced job searching by filtering using different parameters."""
+#     position = job_filter.get_position()
+#     location = job_filter.get_location()
+#     job_type = job_filter.get_job_type()
+#     experience = job_filter.get_experience()
 
-    params = (
-        ('q', position),
-        ('l', location),
-        ('jt', job_type),
-        ('explvl', experience),
-    )
-    response = requests.get('https://www.indeed.com/jobs', params=params)
-    return response
+#     params = (
+#         ('q', position),
+#         ('l', location),
+#         ('jt', job_type),
+#         ('explvl', experience),
+#     )
+#     response = requests.get('https://www.indeed.com/jobs', params=params)
+#     return response
 
 
 def get_job_detail(card):
@@ -52,23 +58,23 @@ def get_job_detail(card):
     job_title = card.find('h2', 'jobTitle').contents[-1].text.strip()
     company_name = card.find('span', 'companyName').text.strip()
     company_location = card.find('div', 'companyLocation').contents[0].strip()
-    job_summary = card.find('div', 'job-snippet').text.strip()
     posted = card.find('span', 'date').text.strip()
     today = datetime.today().strftime("%Y-%m-%d")
+    job_summary = card.find('div', 'job-snippet').text.strip()
     job_url = f"https://www.indeed.com{card.get('href')}"
     try:
         salary = card.find('span', 'salary-snippet').text.strip()
     except AttributeError:
-        salary = ''
+        salary = 'N/A'
 
     job = {
         'job_title': job_title,
         'company_name': company_name,
         'company_location': company_location,
-        'job_summary': job_summary,
         'salary': salary,
         'posted': posted,
         'extracted': today,
+        'job_summary': job_summary,
         'job_url': job_url
     }
     return job
@@ -98,18 +104,11 @@ def save_to_csv(job_list):
 
 
 def main():
-    print('-'*50)
-    print(f'Welcome to Indeed job scraper')
-    print("\nWhat type of job search you want to do?\n")
-    ch = input("Basic or Advanced (default: basic)\n")
+    response = basic_search()
 
-    if ch.lower() == 'advanced':
-        pass
-    elif ch.lower() == 'basic' or ch == '':
-        response = basic_search()
-        html = BeautifulSoup(response.content, 'lxml')
-        job_list = extract_page(html)
-        save_to_csv(job_list)
+    html = BeautifulSoup(response.content, 'lxml')
+    job_list = extract_page(html)
+    save_to_csv(job_list)
 
 if __name__ == '__main__':
     main() 
