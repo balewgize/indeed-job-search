20e2c5198e81f1df281d2d78ccbb9ef9b8306966
Alemnew Marie <alemnewmarie461@gmail.com>
2021-08-12 15:50:30 +0300

    Finalyzing version 1.0 of the program

--- main.py
+++ main.py
@@ -36,19 +36,7 @@ def basic_search():
     )
 
     headers = {
-        'authority': 'www.indeed.com',
-        'cache-control': 'max-age=0',
-        'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
-        'sec-ch-ua-mobile': '?0',
-        'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'sec-fetch-site': 'same-origin',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-user': '?1',
-        'sec-fetch-dest': 'document',
-        'referer': 'https://www.indeed.com/jobs?q=%22Python%20Developer%22&l=Remote&sort=date&fromage=3&start=10&vjk=8998cc80e1d0e19c',
-        'accept-language': 'en-US,en;q=0.9',
+        'user-agent': 'Mozilla/5.0',
     }
 
     response = requests.get('https://www.indeed.com/jobs', headers=headers, params=params)
@@ -109,17 +97,6 @@ def extract_page(html):
 
     return job_list
 
-
-def save_to_csv(job_list):
-    """ Write the list of jobs given to a csv file."""
-    fieldnames = ['job_title', 'company_name', 'company_location', 'salary',
-                'posted', 'extracted', 'job_summary', 'job_url']
-    today = datetime.today().strftime('%Y-%m-%d')
-    with open(f'job-list-{today}.csv', 'w') as file:
-        csv_writer = csv.DictWriter(file, fieldnames=fieldnames)
-        csv_writer.writeheader()
-        csv_writer.writerows(job_list)
-
 def extract_next_pages(current_page):
     """ Extract next result pages from the current page."""
     job_list = []
@@ -140,6 +117,16 @@ def extract_next_pages(current_page):
         time.sleep(random.randint(6, 10))
 
     return job_list
+
+
+def save_to_csv(job_list):
+    """ Write the list of jobs given to a csv file."""
+    fieldnames = job_list[0].keys()
+    today = datetime.today().strftime('%Y-%m-%d')
+    with open(f'job-list-{today}.csv', 'w') as file:
+        csv_writer = csv.DictWriter(file, fieldnames=fieldnames)
+        csv_writer.writeheader()
+        csv_writer.writerows(job_list)
 
 
 def main():
