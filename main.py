701b7107e4e3020d234f4df9b3de8564a33418be
Alemnew Marie <alemnewmarie461@gmail.com>
2021-08-12 15:33:53 +0300

    Extracting next all result pages through navigation

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ Date: August, 2013 E.C
 """
 
 import csv
+import time
 import requests
+import random
 from datetime import datetime
 from bs4 import BeautifulSoup
 
@@ -32,7 +34,24 @@ def basic_search():
         ('sort', sort_by),
         ('fromage', last_three_days),
     )
-    response = requests.get('https://www.indeed.com/jobs', params=params)
+
+    headers = {
+        'authority': 'www.indeed.com',
+        'cache-control': 'max-age=0',
+        'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
+        'sec-ch-ua-mobile': '?0',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-user': '?1',
+        'sec-fetch-dest': 'document',
+        'referer': 'https://www.indeed.com/jobs?q=%22Python%20Developer%22&l=Remote&sort=date&fromage=3&start=10&vjk=8998cc80e1d0e19c',
+        'accept-language': 'en-US,en;q=0.9',
+    }
+
+    response = requests.get('https://www.indeed.com/jobs', headers=headers, params=params)
     return response
 
 
@@ -93,22 +112,45 @@ def extract_page(html):
 
 def save_to_csv(job_list):
     """ Write the list of jobs given to a csv file."""
-    fieldnames = ['job_title', 'company_name', 'company_location', 'job_summary',
-                'salary', 'posted', 'extracted', 'job_url']
+    fieldnames = ['job_title', 'company_name', 'company_location', 'salary',
+                'posted', 'extracted', 'job_summary', 'job_url']
     today = datetime.today().strftime('%Y-%m-%d')
     with open(f'job-list-{today}.csv', 'w') as file:
         csv_writer = csv.DictWriter(file, fieldnames=fieldnames)
         csv_writer.writeheader()
         csv_writer.writerows(job_list)
 
+def extract_next_pages(current_page):
+    """ Extract next result pages from the current page."""
+    job_list = []
+
+    while True:
+        next_page = current_page.find('a', {'aria-label': 'Next'})
+        try:
+            next_url = f"https://www.indeed.com{next_page.get('href')}"
+        except AttributeError:
+            # we reach the end of all job posts (no next button)
+            break
+
+        response = requests.get(next_url)
+        current_page = BeautifulSoup(response.content, 'lxml')
+        jobs = extract_page(current_page)
+        job_list.extend(jobs)
+
+        time.sleep(random.randint(6, 10))
+
+    return job_list
 
 
 def main():
     response = basic_search()
 
-    html = BeautifulSoup(response.content, 'lxml')
-    job_list = extract_page(html)
-    save_to_csv(job_list)
+    first_page = BeautifulSoup(response.content, 'lxml')
+    all_jobs = extract_page(first_page)
+    job_list = extract_next_pages(first_page)
+    all_jobs.extend(job_list)
+
+    save_to_csv(all_jobs)
 
 if __name__ == '__main__':
     main() 
