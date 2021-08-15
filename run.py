6cf8c379c8f61ab2f1f0ba0191c16a699d95ceb4
Alemnew Marie <alemnewmarie461@gmail.com>
2021-08-15 21:46:41 +0300

    A scheduler script to run the job search every Tuesday and Fiday

---
+++ run.py
@@ -0,0 +1,49 @@
+"""
+Schedule to search jobs every Tuesday and Friday at 6:00 AM
+"""
+
+import time
+import schedule
+
+import main
+
+
+def is_connected():
+    """ Check if a device is connected to the internet or not."""
+    import requests
+    try:
+        headers = {
+            'user-agent': 'Mozilla/5.0'
+        }
+        requests.get('https://www.indeed.com', headers=headers, timeout=10)
+        return True
+
+    except (requests.ConnectionError, requests.ConnectTimeout):
+        return False
+
+
+def search_jobs():
+    """ Triggers the main function of main.py"""
+    connected = is_connected()
+    if connected:
+        # search jobs and send an email
+        main.main()
+
+        # once the job search is done, cancel the job task
+        return schedule.CancelJob
+
+""" Since the device may not be always connected to the internet at 06:00 AM,
+    we need to check whether the device has an internet connection every 30 min
+"""
+
+schedule.every(30).minutes.do(search_jobs)
+
+while True:
+    n = schedule.idle_seconds()
+    if n is None:
+        # no more jobs
+        break
+    elif n > 0:
+        # sleep the right amount of time
+        time.sleep(n)
+    schedule.run_pending()
