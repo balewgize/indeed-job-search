1dcb81245fe4d2641f9791d3deff1c5b0f5de74d
Alemnew Marie <alemnewmarie461@gmail.com>
2021-08-22 21:25:10 +0300

    Organizing helper funcions in one directory

---
+++ utils/email_sender.py
@@ -0,0 +1,116 @@
+"""
+It is a python script that enable the user to send emails to a single
+or multiple users using Gmail account in python.
+
+It uses Google App password to gain access to Gmail account.
+"""
+
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+
+class EmailSender():
+    """ Email sender using Gmail account in python.
+    
+    Attributes:
+        gmail_address: 
+            Gmail account used as sender address when sending emails
+        app_password: 
+            Google App password used to gain access to the gmail address given
+    """
+    def __init__(self):
+        self.port = 587
+        self.smtp_server = 'smtp.gmail.com'
+        self.gmail_address, self.app_password = self.user_credentials()
+
+    def user_credentials(self):
+        """ If saved user credential is found, read that credential 
+            and return Gmail address and Google App password, which
+            is used to gain access to the Gmail account.
+
+            Otherwise get user credentials and save to a file for
+            later use so that it will not ask the user to provide
+            credentials every time the program runs.
+
+        returns:
+            gmail_address: used as sender address when sending emails
+            app_password: used to gain access to the gmail address given
+        """
+        import os
+        home_dir = self.get_home_dir()
+        full_path = os.path.join(home_dir, '.email-sender.txt')
+
+        if os.path.exists(full_path):
+            with open(full_path, 'r') as file:
+                gmail_address = file.readline().strip()
+                app_password = file.readline().strip()
+        else:
+            gmail_address = self.get_valid_email()
+            app_password = self.get_app_password()
+
+            with open(full_path, 'w') as file:
+                file.write(f'{gmail_address}\n')
+                file.write(f'{app_password}\n')
+
+        return (gmail_address, app_password)
+    
+    def get_valid_email(self):
+        """ Get valid email address from user."""
+        answer = input('\nEnter your Gmail address: \n').strip()
+        return answer
+    
+    def get_app_password(self):
+        """ Get the 16-digit Google App password to gain access to Gmail."""
+        answer = input('\nEnter the 16-digit Google App password:\n').strip()
+        if len(answer) == 16:
+            return answer
+        else:
+            print('Please provide a valid Google App password.')
+            self.get_app_password()
+
+    def get_home_dir(self):
+        """ Get the home directory of the user based the Operating System."""
+        import os
+        if os.name == 'nt': 
+            return os.path.expanduser('~\\')
+        else:
+            return os.path.expanduser('~/')
+
+    def send(self, to_addr, subject, text, html=''):
+        """ Send an email message to the receiver."""
+        from_addr = self.gmail_address
+
+        message = MIMEMultipart('alternative')
+        message['Subject'] = subject
+        message['From'] = from_addr
+        message['To'] = to_addr
+
+        plain_text = MIMEText(text, 'plain')
+        html_text = MIMEText(html, 'html')
+
+        message.attach(plain_text)
+        message.attach(html_text)
+
+        try:
+            context = ssl.create_default_context()
+
+            print('Connecting to Gmail...')
+            server = smtplib.SMTP(self.smtp_server, self.port)
+            server.starttls(context=context)
+            server.login(self.gmail_address, self.app_password)
+
+            print('Sending message...')
+            server.sendmail(from_addr, to_addr, message.as_string())
+
+            print('\nMessage has been sent.')
+        except Exception as e:
+            print(e)
+            print(f'\n\n{"-"*70}\n'+
+            f'If you keep getting this error message after retrying a few more times,\n'+
+            f'first check your internet connection. If that\'s ok,\n'
+            f'the Gmail address or App password you provide may be incorrect\n'
+            f'So, go to your Home directory and delete ".email-sender.txt" file\n'+
+            f'(It is hidden by default) and run the program again.\n{"-"*70}')
+        finally:
+            server.quit()
---
+++ utils/job_scraper.py
@@ -0,0 +1,96 @@
+"""
+Scrape job post from https://www.indeed.com
+"""
+
+import requests
+import time, random
+from datetime import datetime
+from bs4 import BeautifulSoup
+
+
+class IndeedJobScraper():
+    """ Indeed job scraper class.
+
+    Scrape job posts from www.indeed.com based on user preference and return
+    details about the jobs including: Job title, position, company, location,
+    salary(if any), post date, job summary, and job url.
+    """
+    def __init__(self):
+        self.url = 'https://www.indeed.com/jobs'
+        self.data = {
+            'Job Title': [],
+            'Position': [],
+            'Company': [],
+            'Location': [],
+            'Salary': [],
+            'Posted': [],
+            'Extracted': [],
+            'Summary': [],
+            'Job URL': [],
+        }
+
+    def search_jobs(self, preferences):
+        """ Search jobs based on user preference."""
+        headers = {
+            'user-agent': 'Mozilla/5.0',
+        }
+        response = requests.get(self.url, headers=headers, params=preferences)
+        return response
+
+    def get_job_detail(self, card, position):
+        """ Extract the Job details for a single job."""
+        title_con = card.find('h2', 'jobTitle').contents[-1]
+        job_title = title_con.text.strip()
+
+        company_con = card.find('span', 'companyName')
+        company = company_con.text.strip()
+
+        location_con = card.find('div', 'companyLocation')
+        if len(location_con.contents) == 1:
+            location = location_con.text.strip()
+        else:
+            location = location_con.contents[0].strip()
+
+
+        posted = card.find('span', 'date').text.strip()
+        today = datetime.today().strftime("%Y-%m-%d")
+
+        summary = card.find('div', 'job-snippet').text.strip()
+        job_url = f"https://www.indeed.com{card.get('href')}"
+
+        try:
+            salary = card.find('span', 'salary-snippet').text.strip()
+        except AttributeError:
+            salary = 'N/A'
+
+        return [job_title, position, company, location, salary, posted, today, summary, job_url]
+
+    def extract_page(self, html, position):
+        """ Extract details for all jobs on a single page."""
+        cards = html.find_all('a', 'tapItem')
+
+        for card in cards:
+            job = self.get_job_detail(card, position)
+            for column, value in zip(self.data.keys(), job):
+                self.data[column].append(value)
+
+    def extract_all_pages(self, first_page, position):
+        """ Extract all result pages appeared."""
+        self.extract_page(first_page, position)
+        current_page = first_page
+        
+        while True:
+            next_page = current_page.find('a', {'aria-label': 'Next'})
+            try:
+                next_url = f"https://www.indeed.com{next_page.get('href')}"
+            except AttributeError:
+                # we reach the end of all job posts (no next button)
+                break
+
+            response = requests.get(next_url)
+            current_page = BeautifulSoup(response.content, 'lxml')
+            self.extract_page(current_page, position)
+
+            time.sleep(random.randint(6, 10))
+
+        return self.data
